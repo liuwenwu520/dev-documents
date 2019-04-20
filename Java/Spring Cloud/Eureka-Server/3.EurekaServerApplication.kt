776baf8371d39c460fbee3a0f3fb0994c2b523f1
Wenwu <731876195@qq.com>
2019-04-20 22:51:05 +0800

    更新 3.EurekaServerApplication.kt

--- Java/Spring Cloud/Eureka-Server/3.EurekaServerApplication.kt
+++ Java/Spring Cloud/Eureka-Server/3.EurekaServerApplication.kt
@@ -2,6 +2,7 @@ package org.wenwu.eurekaserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
 @EnableEurekaServer
